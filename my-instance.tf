e85d6c6cde117b3299d6a52cc19f4e5b06db6139
HemaSree1519 <khemasree96@gmail.com>
2019-09-16 14:54:24 +0530

    Add terrafrom to create key pair using tls algorithm

--- my-instance.tf
+++ my-instance.tf
@@ -3,12 +3,48 @@ provider "aws" {
   secret_key = "${var.aws_secret_key}"
   region = "${var.aws_region}"
 }
+resource "aws_security_group" "google-keep" {
+  name = "google-keep"
+  description = "Security Group"
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 8
+    to_port = 0
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_eip" "gk" {
+  instance = "${aws_instance.web_server.id}"
+  vpc      = true
+}
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 
+resource "aws_key_pair" "generated_key" {
+  key_name   = "${var.key_name}"
+  public_key = "${tls_private_key.example.public_key_openssh}"
+}
 resource "aws_instance" "web_server" {
   ami = "ami-03b6f27628a4569c8"
   instance_type = "t2.micro"
   associate_public_ip_address = true
+  security_groups = ["${aws_security_group.google-keep.name}"]
+  key_name      = "${aws_key_pair.generated_key.key_name}"
   tags = {
-    Name = "Terraform_EC2"
+    Name = "Terraform_EC2_with_SG"
   }
 }
